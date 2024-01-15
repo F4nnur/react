dd0d1684b835f6458df44d44e960cc6c5e6f621d
F4nnur <Fann1kF@yandex.ru>
2024-01-15 03:02:10 +0300

    + 33 husky pre-commit

--- src/features/AuthByUsername/UI/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/UI/LoginModal/LoginModal.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { classNames } from 'shared/lib/classNames/classNames';
 import Modal from 'shared/UI/Modal/Modal';
 import LoginForm from 'features/AuthByUsername/UI/LoginForm/LoginForm';
-import cls from './LoginModal.module.scss';
 
 export interface LoginModalProps {
     className?: string;
@@ -17,7 +15,6 @@ const LoginModal = (props: LoginModalProps) => {
     } = props;
     return (
         <Modal
-            className={classNames(cls.LoginModal, {}, [className])}
             isOpen={isOpen}
             onClose={onClose}
         >
